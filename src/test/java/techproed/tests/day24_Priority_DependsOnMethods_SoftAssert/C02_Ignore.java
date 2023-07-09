11e6f5385ad2aced08c7d95d44172aa13ca09656
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-09 02:47:16 +0200

    08.07.2023  TestNG- Annotations-Priority-Ignore

---
+++ src/test/java/techproed/tests/day24_Priority_DependsOnMethods_SoftAssert/C02_Ignore.java
@@ -0,0 +1,49 @@
+package techproed.tests.day24_Priority_DependsOnMethods_SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class C02_Ignore {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    /**
+    Birden fazla test methodu çalıştırmak istediğimizde o anlık gereksiz gördügümüz test methodunu
+    atlamak (ignore) isteyebiliriz. Bunun için @Test notasyonu üstüne yada yanına @Ignore notasyonu
+    eklememiz yeterlidir.@Ignore notasyonu ile atladığımız method konsolda gözükmez raporlamalarda gözükür
+          Eğer bir methodu tamamen iptal etmek isterseniz @Test notasyonundan sonra parametre olarak (enable=false)
+    kullanabilirsiniz.
+     */
+
+    @Test @Ignore
+    public void amazonTest() {
+        driver.get("https://amazon.com");
+    }
+    @Test(enabled = false)
+    public void youtubeTest() {
+        driver.get("https://youtube.com");
+    }
+    @Test
+    public void facebookTest() {
+        driver.get("https://facebook.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+}
