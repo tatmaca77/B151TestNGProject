5852c7b4b31158abee7e6dd1a2ed04d6988e2c12
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-11 01:10:32 +0200

    10.07.2023  TestNG- Annotations-Priority-Ignore-DependsOnMethods

--- src/test/java/techproed/tests/YazilimGelistirmeSüreci/C01_Test01.java
+++ src/test/java/techproed/tests/YazilimGelistirmeSüreci/C01_Test01.java
@@ -57,26 +57,26 @@ public class C01_Test01 {
         Kabul kriterleri ile birlikte yazilmis user story lere frd de denebilir
          */
 
-        /*
+        /**
         Dev kodunu yazarken tester bos durmaz. Test case tasarimini yapar. User Story'e gore test case yazar.
         Expected Result ile Actual Result birbirine esit mi bunu kontrol eder.
 
          */
-        /*
+        /**
         Requirement durumuna gore 5 farkli test case de yazilabilir veya durumlari birlestirip bir test case de yazilabilir
          */
-        /*
+        /**
         Bir gelistirmenin otomasyona dokulebilmesi icin orda yapilan tum hatalarin temizlenmis ve stabil olmasi gerekir.
         Testleri neden otomasyona dokeriz? Zamandan ve dolayisiyla paradan tasarruf saglamak icin.
         Otomasyonun duzgun calismasi icin manuel test sarttir.Manuel test olmadan otomasyon yapma sansimiz yoktur
          */
 
-        /*
+        /**
         Otomasyonda da hata alabiliriz. O zaman bu otomasyonu neden yapariz? Otomasyon onemlidir, ancak gelistirme
         asamasinda herhangi bir hata olup olmadigina otomasyon ile bakilmaz. Oncelikle manual test yapariz ve
         bu hatayi raporlariz. Artik o fonksiyonun hatalardan tamamen arindirilmis oldugu durumda otomasyon testini yapariz.
          */
-        /*
+        /**
         Cok farkli datalarla test yapmamiz gerekiyorsa manuel testte bu konuda zorlaniriz. Bu da otomasyonun avantajidir
         Her canli oncesi veya sprint sonrasi uygulama yazilmis tum case'ler icin test kosulmasi gerekir. Bu durumda regression
         test gerekir.Burda otomasyon buyuk avantajdir.Zaman ve maliyet kaybinin onune gecilir
@@ -85,14 +85,14 @@ public class C01_Test01 {
         Iki gelistirme birbirini etkileyebilir. Canliya cikmadan once yapilan tum testler tekrar yapilir
          */
         //otomasyon daha cok tekrarli islerde one cikiyor olmasi lazim belli bir testi defalarca yapmak gerekiyorsa yada ayni isi farkli girdilerle yapilamsi gerekiyorsa
-        /*
+        /**
         Manual testi yaptik ve hatalardan arindirildi. O zaman neden otomasyona hala neden ihtiyac duyariz?
         Smoke test ile her gun kritik fonksiyonlara bakmamiz gerekir. Bunun icin otomasyon kolaylik saglar.
         Manual test ile yapmamizi zorlastiracak kadar data olabilir. Bu datalari test etmemiz gerekebilir.
         Regression test ile yeni yapilan gelismelerin diger gelismelere olumsuz bir etkisi olmus mu diye bakariz.
          */
 
-        /*
+        /**
         Test Case yazarken test tekniklerine uygun test case yazmaliyiz
         Teknik aynidir ama test datasi degisir.
         Sinir deger analizi, denklik paylarina ayirma, karar tablosu, durum gecis senaryosu test teknigi (belli bir sarta gore durumlarin degistigi)
@@ -100,7 +100,7 @@ public class C01_Test01 {
         Test Tekniklerini minumum test senaryosu ile maksimum alani test etmek icin kullaniriz. Onemli olan hata sayisini en aza indirmektir.
          */
 
-        /*
+        /**
         Manuel de yapilan her islemi once excel de yaziyoruz daha sonra testi kosarken bir video kaydi veya ekran goruntusu almaliyiz.
          */
 
@@ -110,13 +110,13 @@ public class C01_Test01 {
         //Boundary value analysis yas girilecek durumlarda kullanilabilir ya da kilo ile ilgili gruplandirma yapabiliriz. Ornegin 30-50 kilo arasi, 50-70 kilo arasi ve sonrasi
         //Bazen belirli sartlara riayet etmek gerekebilir. Ona uygun test teknigini uygulariz. Her zaman tek bir Teknik kullanmayabiliriz. Birden fazla test teknigini kullanabiliriz. Minimum test ile maksimum sonuc almaya calisma amaci ile hareket ederiz. Ilgili fonksiyon calisiyor mu, buna bakarken hata almayi minimum seviyeye indirmeye calisiriz.
 
-        /*
+        /**
            Smoke testler ==>  Uygulamanin en cok kullanilan en kritik bolumleri ile ilgili yapilan testlerdir.
            E2E Test ==> Sistem Testi - Bir uygulamanin fonksiyonlarinin farkli sekillerde test edilmesi
            Integration Testi = Uygulama arka planda farkli veri yapilariyla calisiyor. Bunlar birbirleriyle uyumlu calisiyor mu bunun testidir
            */
 
-        /*
+        /**
         Test Cesitleri:
         Smoke Test: Bir uygulamanin calisip calismadigi, herhangi bir yerine ulasma problem olup olmadigini test ederiz. Uygulamanin en  cok kullanilan, hata olursa sorun olacak nitelikteki fonksiyonlarini test eder.
         Integration Test: Bir puzzle parcalarini dusunelim. Her bir parcanin farkli sistemlerle etkilesimi soz konusu olur. Birbirinden farkli iki uygulamadan birindeki degisiklik digerini de etkiliyorsa ikisi arasinda bir etkilesim olabilir. Parcalarin birbirleri ile etkilesimleri ve etkileri ile ilgilidir.
@@ -124,7 +124,7 @@ public class C01_Test01 {
          E2E Test: UI’da bir fonksiyon calistiysa onun API’da ve baskaca ortamlarda da calisip calismadigini test etmemizdir. Siteye giristen itibaren tum adimlari test etmemizdir. Bir uctan diger uca kadar testler yapmamizdir.
          */
 
-        /*
+        /**
         Bir uygulamada birbirinden etkilenen bolumlerin test edilmesi -minor regresyon
         Uygulamanin tamaminin test edilmesi -major regresyon
          */
