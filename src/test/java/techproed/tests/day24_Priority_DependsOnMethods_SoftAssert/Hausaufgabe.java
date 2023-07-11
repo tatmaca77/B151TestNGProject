5afa0fad246cf22ff021f616c1119af5c2092be5
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-11 03:29:53 +0200

    10.07.2023  TestNG- Annotations-Priority-Ignore-DependsOnMethods

---
+++ src/test/java/techproed/tests/day24_Priority_DependsOnMethods_SoftAssert/Hausaufgabe.java
@@ -0,0 +1,30 @@
+package techproed.tests.day24_Priority_DependsOnMethods_SoftAssert;
+
+public class Hausaufgabe {
+
+    /**
+    Soft Assert Class Work
+Yeni bir Class Olusturun : C03_SoftAssert
+1. “http://zero.webappsecurity.com/” Adresine gidin
+2. Sign in butonuna basin
+3. Login kutusuna “username” yazin
+4. Password kutusuna “password” yazin
+5. Sign in tusuna basin
+6. Online banking menusu icinde Pay Bills sayfasina gidin
+7. “Purchase Foreign Currency” tusuna basin
+8. “Currency” drop down menusunden Eurozone’u secin
+9. soft assert kullanarak "Eurozone (Euro)" secildigini test edin
+10.soft assert kullanarak DropDown listesinin su secenekleri oldugunu test
+edin "Select One", "Australia (dollar)", "Canada (dollar)","Switzerland
+(franc)","China (yuan)","Denmark (krone)","Eurozone (euro)","Great Britain
+(pound)","Hong Kong (dollar)","Japan (yen)","Mexico (peso)","Norway
+(krone)","New Zealand (dollar)","Sweden (krona)","Singapore
+(dollar)","Thailand (baht)"
+     */
+
+
+
+
+
+
+}
