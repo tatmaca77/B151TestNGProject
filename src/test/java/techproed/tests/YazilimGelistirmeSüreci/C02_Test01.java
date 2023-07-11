cd02bee4ac6c6a8b5c999318bee4d5cb8c017671
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-12 01:55:04 +0200

    11.07.2023  TestNG- Annotations-Priority-Ignore-DependsOnMethods

---
+++ src/test/java/techproed/tests/YazilimGelistirmeSüreci/C02_Test01.java
@@ -0,0 +1,29 @@
+package techproed.tests.YazilimGelistirmeSüreci;
+
+public class C02_Test01 {
+
+
+    /**
+    Allover Commerce
+Sag ust kosede; sign in/ register button -giris yap, uye ol
+Sign In icinde bir kullanici olarak ya da satici olarak giris yapabiliriz. Iki rol var. user ve vendor
+Fakemail.net sitesi bir saatligine bir mail sagliyor bize
+Oradan uretilen maili alip Allover Commerce Sign In icerisinde bu email addresini kullandik
+Kayit onayi sonrasi user olarak siteye girdik. Ardindan myaccount kismina gelip bilgileri girdik.
+Sitede bir urun incelemek icin search tool’da urun aratiyoruz. Urunleri tiklayarak ozelliklerini gorebiliyoruz.
+Add to Cart ile sepete urun ekliyoruz.
+Istek listesine urun eklersek kalp sekline tiklayinca listemizi goruyoruz.
+Terazi isareti ile urun karsilastirmasi yapabiliyoruz. Bir urunu karsilastirmada kullanmak icin terazi isaretine tikliyoruz. Sonra basak bir urun gorunce ve karsilastirma yapilsin diye istedigimizde tekrar terazi isaretine tikliyoruz.
+Billing Details kismi odeme icin gerekli bilgileri iceriyor. Bunu daha sonra dolduracagiz.
+Satin alma islemini test edecegiz.
+Biz daha sonra adres tanimlamalarini yapacagiz. Odeme adres tanimlama ile mumkun olacak
+Birinci rolumuzde user olduk. Buradaki menuler user icin olan menuler
+Ikinci rolumuzde bir urun saticisi olabiliriz. Sayfaya satici olarak girebilmek icin Sign In’e girecegiz.
+Satici icin farkli menuler var.
+Products bolumune giriyoruz. MyStore’da product Add new’e tiklariz. Urun bilgilerini fiyatini gireriz
+Simple product devaminda ismi, ucreti girecegiz. Isim olarak logo’yu girdik.  Submit’e tiklayinca yeni urun ekleriz.
+Products’a tekrar tiklayinca ekledigimiz urun gelmis mi gorebiliriz. Orders’da siparisleri aliyoruz.
+Coupons ise urunlerde indirim yapmak istersek bunun icin kullaniyoruz. Indirim miktari ve suresini giriyoruz. Code, Type, Amount, Usage Limit, Expiry Date basliklari altini doldurup kupon olusturuyoruz. Bu sekilde kuponlar olusturuyoruz.
+Vendor Registration bu sitede satici olarak kayit yapmak ile ilgili.
+     */
+}
