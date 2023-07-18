c41782cd64386a08b6d4e72d0696914bbfb84abb
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-18 17:09:56 +0200

    17.07.2023  TestNG- Driver- Properties

--- src/test/java/techproed/utilities/ConfigReader.java
+++ src/test/java/techproed/utilities/ConfigReader.java
@@ -6,23 +6,24 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    /**
+
+    /**  configuration.properties icindeki verileri cekebilmemiz okuyabilmemiz icin ConfigReader olusturduk. "fis"
     .properties uzantılı dosyaya erişebilmemiz için Properties class'ından obje oluşturmamız gerekir.
     bu oluşturduğumuz obje ile akışa aldığımız dosya yolunu load(fis) methodu ile properties dosyasındaki
-    key değerini return edebiliriz
+    key değerini return edebiliriz.
+     Bunu yapmak icin parametreli bir method olustururuz ve girdigimiz key'in degerini bize döndürür.
      */
+
     static Properties properties;
-    static {
+
+    public static String getProperty (String key) {
         try {
             FileInputStream fis = new FileInputStream("configuration.properties");
             properties = new Properties();
-            properties.load(fis);//--> fis'in okuduğu bilgileri properties'e yükler
+            properties.load(fis); // --> fis 'in okudugu bilgileri properties'e yükler.
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-    public static String getProperty(String key) {
-
-        return properties.getProperty(key);//-->getProperty(key) methodu properties dosyasındaki key'in değerini verir
+        return properties.getProperty(key); // --> String olarak girdigim key'in degerini return eder.
     }
 }
