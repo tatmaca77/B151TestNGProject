b44aab95838103376e31e27a48b79d81dfff99fc
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-18 17:15:37 +0200

    17.07.2023  TestNG- Driver- Properties

--- src/test/java/techproed/tests/day25_POM/C02_PropertiesKullanimi.java
+++ src/test/java/techproed/tests/day25_POM/C02_PropertiesKullanimi.java
@@ -1,4 +1,21 @@
 package techproed.tests.day25_POM;
 
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigReader;
+
 public class C02_PropertiesKullanimi {
+
+    @Test
+    public void test01() {
+        String amazonUrl = ConfigReader.getProperty("amazonUrl");
+        String username = ConfigReader.getProperty("username");
+        String password = ConfigReader.getProperty("password");
+        System.out.println(amazonUrl);
+        System.out.println(username);
+        System.out.println(password);
+    }
+
+
+
+
 }
