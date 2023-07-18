ab91b21ce1ec639d71a492274f2c1b07e71626fa
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-18 17:42:28 +0200

    17.07.2023  TestNG- Driver- Properties

--- src/test/java/techproed/tests/day25_POM/C03_PropertiesKullanimi.java
+++ src/test/java/techproed/tests/day25_POM/C03_PropertiesKullanimi.java
@@ -11,39 +11,41 @@ public class C03_PropertiesKullanimi {
 
     @Test
     public void amazonTest() {
-        //amazon sayfasina gidelim
+        /** amazon sayfasina gidelim */
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
 
 
-        // basligin amazon icerdigini dogrulayalim
+        /** basligin amazon icerdigini dogrulayalim */
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = ConfigReader.getProperty("expectedTitle");
         Assert.assertTrue(actualTitle.contains(expectedTitle));
 
 
 
-        // arama kutusunda iphone aratalim
+        /** arama kutusunda iphone aratalim */
         Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys(ConfigReader.getProperty("searchText"),Keys.ENTER);
 
 
-        // sayfayi kapatalim
+        /** sayfayi kapatalim */
         Driver.closeDriver();
 
 
-        // google sayfasina gidelim
+        /** google sayfasina gidelim */
         Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
 
 
-        // Sayfayi kapatalim
+        /** Sayfayi kapatalim */
         Driver.closeDriver();
 
 
-        //facebook sayfasina gidelim
+        /** facebook sayfasina gidelim */
         Driver.getDriver().get("https://www.facebook.com"); // Bu sekilde de olur. Illa ConfigReader'dan almaya gerek yok.
 
         // Eger ben 1 kere bu Url'e gideceksem bu sekilde gitmek mantiklidir. ConfigReader'da olma zorunlulugu yoktur.
 
-        // Sayfayi kapatalim
+
+
+        /** Sayfayi kapatalim */
         Driver.closeDriver();
 
 
