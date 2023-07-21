34ac53d501f32220c5801381a24d3a78418562b8
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-22 00:06:12 +0200

    21.07.2023  TestNG- DataProvider

---
+++ src/test/java/techproed/utilities/DataProviderUtil.java
@@ -0,0 +1,10 @@
+package techproed.utilities;
+
+public class DataProviderUtil {
+
+    @DataProvider
+    public static Object[][] sehirVerileri() {
+        return new Object[][]{{"Ankara","IcAnadolu","06"},{"Izmir","Ege","35"},{"Diyarbakır","Doğu","21"}};
+    }
+
+}
